64f41175d69c5dc92c9325459986764b3bb63704
KingRainbow44 <27646710+KingRainbow44@users.noreply.github.com>
2021-02-24 09:28:32 -0800

    initial commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,83 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'http://exchange.ricedefi.com/',
+      },
+      {
+        label: 'Liquidity',
+        href: 'http://exchange.ricedefi.com/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Kitchen',
+    icon: 'PoolIcon',
+    href: '/nests',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x8f2A3288c7F627F9A9C0E08a29CE8C829c1c7459',
+      },
+    ],
+  },
+  // {
+  //   label: 'IFO',
+  //   icon: 'IfoIcon',
+  //   href: '/ifo',
+  // },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      // {
+      //   label: 'Voting',
+      //   href: 'https://voting.pancakeswap.finance',
+      // },
+      {
+        label: "Github",
+        href: "https://github.com/ricefinance/",
+      },
+      {
+        label: "Docs",
+        href: "https://ricefinance.gitbook.io/rice-finance/",
+      },
+    ],
+  },
+]
+
+export default config
---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    56: '0x8f2A3288c7F627F9A9C0E08a29CE8C829c1c7459',
+    97: '',
+  },
+  masterChef: {
+    56: '0x426c88A5156b9aD6f76377A17AAb19c5c7017053',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+}
